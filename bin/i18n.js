939b596e5c11e110120be1d326994a8fc681a361
Yohan Boniface <yb@enix.org>
2013-11-20 22:38:13 +0100

    Add a json mode for easing use of Transifex

--- bin/i18n.js
+++ bin/i18n.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 var argv = require('optimist')
-           .usage('node i18n.js --dir_path={somepath} --locale_dir_path={somepath} --locale_codes={nl,fr,es}')
-           .demand(['dir_path','locale_dir_path', 'locale_codes'])
+           .usage('node i18n.js --dir_path={somepath} --locale_dir_path={somepath} --locale_codes={nl,fr,es} --mode={js|json}')
+           .demand(['dir_path','locale_dir_path', 'locale_codes', 'mode'])
+           .default('mode', 'js')
            .argv;
 var UglifyJS = require("uglify-js");
 var fs = require("fs");
@@ -10,7 +11,7 @@ var path = require("path");
 var dir_path = argv.dir_path,
     locale_dir = argv.locale_dir_path,
     locale_codes = argv.locale_codes.split(','),
-    locales = {},
+    mode = argv.mode,
     files = fs.readdirSync(dir_path),
     ast = null,
     code = "",
@@ -29,31 +30,46 @@ ast.walk(new UglifyJS.TreeWalker(function (node) {
 }));
 strings.sort();
 
-/* Needed to eval the locale files */
-L = {
-    registerLocale: function (name, strings) {
-        locales[name] = strings;
-    }
-};
-locale_codes.forEach(function (locale_code) {
-    locales[locale_code] = {};
-    var locale_path = path.join(locale_dir, locale_code + ".js");
+var toJS = function (locale_code, locale_path) {
+    /* Needed to eval the locale files */
+    L = {
+        registerLocale: function (name, strings) {
+            translations = strings;
+        }
+    };
+    var translations = {},
+        raw_content = "var " + locale_code + " = ";
     if (fs.existsSync(locale_path)) {
         // Will call our monkeypatched registerLocale
         eval(fs.readFileSync(locale_path, "utf8"));
     }
-    var translations = locales[locale_code],
-        missing = {},
-        locale_raw_content = "var " + locale_code + " = ";
     strings.forEach(function (str) {
         if (!translations[str]) {
             translations[str] = "";
-            missing[str] = "";
         }
     });
-    locale_raw_content += JSON.stringify(translations, null, 4);
-    locale_raw_content += ";\n\n";
-    locale_raw_content += 'L.registerLocale("' + locale_code + '", '+ locale_code + ');';
+    raw_content += JSON.stringify(translations, null, 4);
+    raw_content += ";\n\n";
+    raw_content += 'L.registerLocale("' + locale_code + '", '+ locale_code + ');';
+    return raw_content;
+};
+
+var toJSON = function (locale_code, locale_path) {
+    var translations = {};
+    if (fs.existsSync(locale_path)) {
+        translations = JSON.parse(fs.readFileSync(locale_path, "utf8"));
+    }
+    strings.forEach(function (str) {
+        if (!translations[str]) {
+            translations[str] = "";
+        }
+    });
+    return JSON.stringify(translations, null, 4);
+};
+locale_codes.forEach(function (locale_code) {
+    var locale_path = path.join(locale_dir, locale_code + "." + mode);
+    var func = (mode === "json")? toJSON: toJS;
+    var raw_content = func(locale_code, locale_path);
     process.stdout.write('Writing file for locale "' + locale_code + '"\n');
-    fs.writeFileSync(locale_path, locale_raw_content, 'utf8');
+    fs.writeFileSync(locale_path, raw_content, 'utf8');
 });
