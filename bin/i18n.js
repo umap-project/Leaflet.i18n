7bac9c9c257d86872468a15ad7cfaf3e2d2f1592
Artemiy <rendrom@gmail.com>
2017-05-26 10:53:40 +0800

    UglifyJS 3.x support
    no `UglifyJS.parse` function since 3.? version of UglifyJS 
    
    work with uglify-js^3.0.11 and ^2.3.6

--- bin/i18n.js
+++ bin/i18n.js
@@ -24,17 +24,17 @@ dirs.forEach(function (dir_path) {
         code += fs.readFileSync(path.join(dir_path, file), "utf8");
     });
 })
-ast = UglifyJS.parse ? UglifyJS.parse : UglifyJS.minify(code, {
-                parse: {
-                    expression: true
-                },
-                compress: false,
-                mangle: false,
-                output: {
-                    ast: true,
-                    code: false
-                }
-            }).ast;
+ast = UglifyJS.parse ?
+    UglifyJS.parse(code) :
+    UglifyJS.minify(code, {
+        parse: {},
+        compress: false,
+        mangle: false,
+        output: {
+            ast: true,
+            code: false  // optional - faster if false
+        }
+    }).ast;
 
 ast.walk(new UglifyJS.TreeWalker(function (node) {
     if (node instanceof UglifyJS.AST_Call && node.expression.property == "_") {
